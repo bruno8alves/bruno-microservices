ec4ed7a9602259187168493b94de06cb8406c968
Bruno Alves <bruno.8.alves@gmail.com>
2022-05-19 12:52:29 -0700

    Initial commit

---
+++ demo/src/test/java/ca/com/bruno/demo/StartupTests.java
@@ -0,0 +1,13 @@
+package ca.com.bruno.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
